373260bb126ed46361cdf0e69b7ff77fe7b47bcc
gucongyi <gucongyii@163.com>
2017-09-21 09:57:43 +0800

    SceneController

---
+++ SceneController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+public class SceneController : UnitySingleton<SceneController>
+{
+    public const float BattleScenePercent = 0.6f;
+    public void LoadSceneAynsc(string sSceneName, System.Action<float> progress = null, System.Action completed = null)
+    {
+        StartCoroutine(LoadSceneCorotine(sSceneName, progress, completed));
+    }
+
+    IEnumerator LoadSceneCorotine(string sSceneName, System.Action<float> progress = null, System.Action completed = null)
+    {
+        
+        float displayProgress = 0;
+        if ("Battle" == sSceneName)
+        {
+            displayProgress = BattleScenePercent;
+        }
+        AsyncOperation ao = Application.LoadLevelAsync(sSceneName);
+        while (!ao.isDone)
+        {
+            displayProgress += 0.01f;
+            if ("Battle" == sSceneName)
+            {
+                float aoProgress = ao.progress * (1 - BattleScenePercent) + BattleScenePercent;
+                displayProgress = displayProgress < aoProgress ? displayProgress : aoProgress;
+            }
+            else
+            {
+                displayProgress = displayProgress < ao.progress ? displayProgress : ao.progress;
+            }
+            if ("Battle" == sSceneName)
+            {
+                if (ao.progress>0.8f)
+                {
+                    displayProgress = 1f;
+                }
+                if (progress != null)
+                {
+                    progress(displayProgress);
+                }
+                yield return new WaitForEndOfFrame();
+            }else if (progress != null)
+            {
+                progress(displayProgress);
+            }
+            
+            yield return new WaitForEndOfFrame();
+        }
+        if ("Battle" == sSceneName)
+        {
+            if (completed != null)
+            {
+                completed();
+            }
+        }
+        else
+        {
+            while (displayProgress < 1f)
+            {
+                displayProgress += 0.01f;
+
+                if (progress != null)
+                {
+                    progress(displayProgress);
+                }
+                yield return new WaitForEndOfFrame();
+            }
+            if (completed != null)
+            {
+                completed();
+            }
+        }
+        
+    }
+    
+
+}
